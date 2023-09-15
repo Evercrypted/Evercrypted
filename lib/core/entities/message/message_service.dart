09c4f0e01061dcd801a2c613370eec44c4a44b6f
iraklikori <iraklikori@gmail.com>
2023-09-15 13:18:40 +0400

    commit

--- lib/core/entities/message/message_service.dart
+++ lib/core/entities/message/message_service.dart
@@ -1,8 +1,6 @@
 import 'dart:async';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:evercrypted/core/entities/chat/chat_model.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:isar/isar.dart';
 
 import 'message_isar.dart';
@@ -11,34 +9,34 @@ class MessageService {
   StreamSubscription? fbListener;
 
   void startListeningAndWritingToDB(Chat chatRoom, int? lastMessageTime) async {
-    final userId = FirebaseAuth.instance.currentUser?.uid;
-    final participantsToListenTo =
-        chatRoom.participants!.where((element) => element != userId).toList();
-    final messagesCollection = FirebaseFirestore.instance
-        .collection('chatRooms')
-        .doc(chatRoom.uid)
-        .collection('messages');
-    final messageRequests = lastMessageTime != null
-        ? messagesCollection
-            .where('authorId', whereIn: participantsToListenTo)
-            .where('createdAtMSE', isGreaterThan: lastMessageTime)
-        : messagesCollection.where('authorId', isNotEqualTo: userId);
-    final isar = Isar.getInstance();
-    fbListener = messageRequests.snapshots().listen((event) async {
-      for (var change in event.docChanges) {
-        if (change.type == DocumentChangeType.added) {
-          final message = null;
-          message.chatId = chatRoom.uid;
-          final isInDb =
-              isar!.messages.where().uidEqualTo(message.fbUid).countSync() > 0;
-          if (!isInDb) {
-            await isar.writeTxn(() async {
-              await isar.messages.put(message);
-            });
-          }
-        }
-      }
-    });
+    // final userId = FirebaseAuth.instance.currentUser?.uid;
+    // final participantsToListenTo =
+    //     chatRoom.participants!.where((element) => element != userId).toList();
+    // final messagesCollection = FirebaseFirestore.instance
+    //     .collection('chatRooms')
+    //     .doc(chatRoom.uid)
+    //     .collection('messages');
+    // final messageRequests = lastMessageTime != null
+    //     ? messagesCollection
+    //         .where('authorId', whereIn: participantsToListenTo)
+    //         .where('createdAtMSE', isGreaterThan: lastMessageTime)
+    //     : messagesCollection.where('authorId', isNotEqualTo: userId);
+    // final isar = Isar.getInstance();
+    // fbListener = messageRequests.snapshots().listen((event) async {
+    //   for (var change in event.docChanges) {
+    //     if (change.type == DocumentChangeType.added) {
+    //       final message = null;
+    //       message.chatId = chatRoom.uid;
+    //       final isInDb =
+    //           isar!.messages.where().uidEqualTo(message.fbUid).countSync() > 0;
+    //       if (!isInDb) {
+    //         await isar.writeTxn(() async {
+    //           await isar.messages.put(message);
+    //         });
+    //       }
+    //     }
+    //   }
+    // });
   }
 
   Future<List<Message>> getMessagesFromDB(int pageKey, int pageSize) async {
--- lib/core/services/image_upload.dart
+++ lib/core/services/image_upload.dart
@@ -1,5 +1,1 @@
-import 'package:firebase_storage/firebase_storage.dart';
 
-class ImageUploadService {
-  final storage = FirebaseStorage.instance;
-}
